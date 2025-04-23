3d250b52825ff41ca9351cdf8327e89f34c672f5
andbilous <andbilous@gmail.com>
2025-04-23 10:52:10 +0200

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            We Automate Growth with AI-Powered Funnels
           </a>{" "}
         </div>
 
